d68e297139d077ad65f6d5937597419a1e31acc9
Ng Guoyou <fauxface+github@gmail.com>
2013-12-05 10:29:48 +0800

    Move object loader into a module

--- lib/bot/core.rb
+++ lib/bot/core.rb
@@ -22,6 +22,9 @@ module Bot
   end
 
   class Core
+    require_relative 'core/object_loader'
+    include Bot::Core::ObjectLoader
+
     attr_reader :adapters, :plugins
     START_TIME = Time.now
 
@@ -41,43 +44,6 @@ module Bot
       load_objects('adapter')
       load_objects('plugin')
       Bot.log.info "#{@adapters.length} adapter(s) and #{@plugins.length} plugin(s) loaded."
-    end
-
-    def get_objects_dir(type)
-      File.join(Bot::ROOT_DIR, @settings["#{type}s_dir".to_sym])
-    end
-
-    def load_objects(type)
-      objects_dir = get_objects_dir(type)
-      Dir.foreach(objects_dir) do |f|
-        next if f == '.' || f == '..'
-        load_curry(type).call(f) if File.directory?(File.join(objects_dir, f))
-      end
-    end
-
-    # Loads object type and adds a reference to it
-    def load_curry(type)
-      Proc.new do |f|
-        begin
-          Bot.log.info "Loading #{type} #{f}..."
-          path =  File.join(get_objects_dir(type), f.to_s)
-          load File.join(path, "#{f}.rb")
-          # Initialize the loaded object
-          object = Bot.module_eval("#{type.capitalize}").const_get(f.capitalize).new
-          # And store a reference to that object in @types (eg. @plugins)
-          instance_eval("@#{type}s")[f.to_sym] = object
-        rescue => e
-          Bot.log.warn "Failed to load #{f} - #{e}\n\t#{e.backtrace.join("\n\t")}"
-        end
-      end
-    end
-
-    def load_adapter(adapter)
-      load_curry('adapter').call(adapter)
-    end
-
-    def load_plugin(plugin)
-      load_curry('plugin').call(plugin)
     end
 
     def reload(type, name=nil)
---
+++ lib/bot/core/object_loader.rb
@@ -0,0 +1,39 @@
+# Methods for loading adapters/plugins with blacklists/whitelists
+module Bot::Core::ObjectLoader
+  def get_objects_dir(type)
+    File.join(Bot::ROOT_DIR, @settings["#{type}s_dir".to_sym])
+  end
+
+  def load_objects(type)
+    objects_dir = get_objects_dir(type)
+    Dir.foreach(objects_dir) do |f|
+      next if f == '.' || f == '..'
+      load_curry(type).call(f) if File.directory?(File.join(objects_dir, f))
+    end
+  end
+
+  # Loads object type and adds a reference to it
+  def load_curry(type)
+    Proc.new do |f|
+      begin
+        Bot.log.info "Loading #{type} #{f}..."
+        path =  File.join(get_objects_dir(type), f.to_s)
+        load File.join(path, "#{f}.rb")
+        # Initialize the loaded object
+        object = Bot.module_eval("#{type.capitalize}").const_get(f.capitalize).new
+        # And store a reference to that object in @types (eg. @plugins)
+        instance_eval("@#{type}s")[f.to_sym] = object
+      rescue => e
+        Bot.log.warn "Failed to load #{f} - #{e}\n\t#{e.backtrace.join("\n\t")}"
+      end
+    end
+  end
+
+  def load_adapter(adapter)
+    load_curry('adapter').call(adapter)
+  end
+
+  def load_plugin(plugin)
+    load_curry('plugin').call(plugin)
+  end
+end
