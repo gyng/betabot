cfc39ff08a210bdc467a49ddc2330fea78c4f201
Ng Guoyou <fauxface+github@gmail.com>
2014-01-16 19:36:09 +0800

    Get the only IRC spec passing

--- spec/bot/adapter/irc_spec.rb
+++ spec/bot/adapter/irc_spec.rb
@@ -7,23 +7,19 @@ require 'adapters/irc/message'
 
 describe Bot::Adapter::Irc::Handler do
   before do
-    @irc = Bot::Adapter::Irc::Handler.new true
+    Bot::ROOT_DIR = '.'
+    @adapter = Bot::Adapter::Irc.new(true)
+    @handler = Bot::Adapter::Irc::Handler.new(@adapter, {})
   end
 
   it 'parses PRIVMSG' do
     data = ':nick!real@test.com PRIVMSG #test :test'
-    m = @irc.parse_data(data)
+    m = @handler.parse_data(data)
     expect(m.sender).to eq('nick')
     expect(m.real_name).to eq('real')
     expect(m.hostname).to eq('test.com')
     expect(m.type).to eq(:privmsg)
     expect(m.channel).to eq('#test')
     expect(m.text).to eq('test')
-  end
-
-  it 'registers with the server when connected' do
-    Bot::Adapter::Irc::Handler.stub(:send_data).and_return(true)
-    @irc.connection_completed
-    Bot::Adapter::Irc::Handler.any_instance.should_receive(:register)
   end
 end
