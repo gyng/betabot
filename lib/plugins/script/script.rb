caaa32cfd72aa843911790357c02aadc3dacc07f
Ng Guoyou <fauxface+github@gmail.com>
2014-01-20 01:28:05 +0800

    Add script plugin.

---
+++ lib/plugins/script/script.rb
@@ -0,0 +1,37 @@
+class Bot::Plugin::Script < Bot::Plugin
+  require 'timeout'
+
+  def initialize(bot)
+    @s = {
+      trigger: { script: [:call, 5, 'Evals the script in a binding with $SAFE = 2 and a timeout.'] },
+      subscribe: false,
+      timeout: 3
+    }
+    super(bot)
+  end
+
+  def call(m)
+    Thread.start do
+      begin
+        $SAFE = 2
+        b = Context.new.get_binding
+
+        Timeout::timeout(@s[:timeout]) do
+          m.reply "=> #{b.eval(m.args[0..-1].join(' ')).inspect}"
+        end
+      rescue Exception => e
+        m.reply e
+      end
+    end
+  rescue Exception => e
+    Bot.log.warn(e)
+    Bot.log.warn(e.backtrace.join("\n"))
+    m.reply e
+  end
+
+  class Bot::Plugin::Script::Context
+    def get_binding
+      return binding()
+    end
+  end
+end
