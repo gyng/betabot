cd0794b1b4cf83d964e5712f86d2b06c38c22c32
Ng Guoyou <fauxface+github@gmail.com>
2014-05-01 00:51:44 +0800

    Core: Move logger back into separate file.

--- lib/bot/core.rb
+++ lib/bot/core.rb
@@ -6,29 +6,7 @@ module Bot
 
   require_relative 'adapter'
   require_relative 'plugin'
-
-  class << self
-    attr_accessor :log
-
-    def log
-      if !@log
-        @log = Logger.new(STDOUT)
-        @log.level = Logger::WARN if ENV['TEST'] # Set in spec_helper.rb
-        @log.formatter = -> severity, datetime, progname, message do
-          color = case severity
-            when 'FATAL';   :red
-            when 'ERROR';   :red
-            when 'WARN';    :red
-            when 'INFO';    :gray
-            when 'DEBUG';   :gray
-            when 'UNKNOWN'; :gray
-          end
-          "#{(severity[0] + ' ' + datetime.to_s + ' | ').send(color)}#{message}\n"
-        end
-      end
-      @log
-    end
-  end
+  require_relative 'util/logger'
 
   class Core
     require_relative 'database'
