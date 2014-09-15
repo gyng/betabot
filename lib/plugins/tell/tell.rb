aae30e03592bb14021029661d8de630cca9e5c9c
Ng Guoyou <fauxface+github@gmail.com>
2014-09-15 09:37:49 +0800

    Tell: Fix sentences, multiple tells, add time

--- lib/plugins/tell/tell.rb
+++ lib/plugins/tell/tell.rb
@@ -11,14 +11,17 @@ class Bot::Plugin::Tell < Bot::Plugin
   end
 
   def tell(m)
-    @stored_messages[m.args[0]] = { message: m.args[1], from: m.sender }
+    @stored_messages[m.args[0]] = [] if @stored_messages[m.args[0]].nil?
+    @stored_messages[m.args[0]].push({ message: m.args[1..-1].join(' '), from: m.sender, at: Time.now.to_i })
     m.reply("Okay!")
   end
 
   def receive(m)
     if m.type == :privmsg || m.type == :join
-      tell = @stored_messages.delete(m.sender)
-      m.reply("#{m.sender}: #{tell[:from]} wanted to tell you ``#{tell[:message]}''") if !tell.nil?
+      tells = @stored_messages.delete(m.sender)
+      tells.each do |t|
+        m.reply("#{m.sender}: #{t[:from]} wanted to tell you ``#{t[:message]}'', #{((Time.now.to_i - t[:at]) / 60.0 / 60.0).round(2)} hours ago")
+      end
     end
   end
 end
