e6c9ed40b6f2880cca15957acfa9a06349231ecb
Ng Guoyou <fauxface+github@gmail.com>
2014-11-29 20:45:01 +0800

    Update for RSpec3

--- spec/bot/adapter_spec.rb
+++ spec/bot/adapter_spec.rb
@@ -1,16 +1,16 @@
 require 'bot/adapter'
 
 describe Bot::Adapter do
-  pending 'loads settings from JSON' do
+  skip 'loads settings from JSON' do
   end
 
-  pending 'keeps track of registered plugins' do
+  skip 'keeps track of registered plugins' do
     # Hooks, auth, read every line etc.
   end
 
-  pending 'handles exceptions' do
+  skip 'handles exceptions' do
   end
 
-  pending 'passes message to core' do
+  skip 'passes message to core' do
   end
 end
