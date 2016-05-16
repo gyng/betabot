745b1e31493d9bb1fc739d83c4f39215bb5e3340
Ng Guoyou <fauxface+github@gmail.com>
2016-05-17 00:50:36 +0800

    Unicode: Handle empty results and multiple queries

--- lib/plugins/unicode/unicode.rb
+++ lib/plugins/unicode/unicode.rb
@@ -8,7 +8,7 @@ class Bot::Plugin::Unicode < Bot::Plugin
       trigger: {
         u: [:find, 0, 'u <query> - Search for characters by Unicode description and emoji aliases.'],
         uid: [:name, 0, 'uid <query> - Returns the name of the Unicode characters of a string.'],
-        e: [:emoji, 0, 'e <emoji> - Search for emoji by alias (exact match)']
+        e: [:emoji, 0, 'e <emoji +> - Search for emoji by aliases (exact match)']
       },
       subscribe: false
     }
@@ -16,8 +16,8 @@ class Bot::Plugin::Unicode < Bot::Plugin
     super(bot)
   end
 
-  def find(m=nil)
-    if m.args.length == 0
+  def find(m = nil)
+    if m.empty?
       m.reply @s[:trigger][:u][2]
     elsif m.args[0].length <= 2
       m.reply "Pattern too short (< 3)"
@@ -26,18 +26,18 @@ class Bot::Plugin::Unicode < Bot::Plugin
       emoji_match = find_emoji(m.args[0])
       emoji_match = emoji_match.raw if emoji_match
 
-      if matches.length > 0
+      if !matches.empty?
         m.reply(emoji_match)
         m.reply(matches[0..999].join)
         m.reply "#{matches.length} codepoints found. #{matches.length - 1000} omitted." if matches.length > 1000
       else
-        m.reply "No matches found."
+        m.reply 'No matches found.'
       end
     end
   end
 
-  def name(m=nil)
-    if m.args.length == 0
+  def name(m = nil)
+    if m.empty?
       m.reply @s[:trigger][:uid][2]
     else
       m.reply(
@@ -56,8 +56,16 @@ class Bot::Plugin::Unicode < Bot::Plugin
     Emoji.find_by_alias(query)
   end
 
-  def emoji(m=nil)
-    match = find_emoji(m.args[0].downcase)
-    m.reply(match && match.raw)
+  def emoji(m = nil)
+    results = m.args.map do |q|
+      result = find_emoji(q.downcase)
+      result.raw if result
+    end.reject(&:nil?)
+
+    if results.empty?
+      m.reply 'No matches found.'
+    else
+      m.reply(results.join)
+    end
   end
 end
