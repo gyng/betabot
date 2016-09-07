f138f16b9636b349841cf366af4c01d5d11a53d2
Ng Guoyou <fauxface+github@gmail.com>
2016-09-08 02:22:23 +0800

    Core: Move common message attributes to Core::Message

--- lib/adapters/irc/message.rb
+++ lib/adapters/irc/message.rb
@@ -1,15 +1,10 @@
 class Bot::Adapter::Irc::Message < Bot::Core::Message
-  attr_accessor :adapter
-  attr_accessor :sender
   attr_accessor :real_name
   attr_accessor :hostname
   attr_accessor :type
   attr_accessor :channel
-  attr_accessor :text
   attr_accessor :raw
-  attr_accessor :time
   attr_accessor :origin
-  attr_accessor :internal_type
 
   def initialize
     yield self if block_given?
--- lib/bot/core/message.rb
+++ lib/bot/core/message.rb
@@ -1,8 +1,10 @@
 class Bot::Core::Message
-  attr_accessor :sender
+  attr_accessor :adapter
   attr_accessor :hostname
   attr_accessor :internal_type # Server or client message, distinct from adapter's internal message :type
+  attr_accessor :sender
   attr_accessor :text
+  attr_accessor :time
 
   def initialize
     yield self if block_given?
