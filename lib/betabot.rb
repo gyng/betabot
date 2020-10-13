eeb3cb3876fe6d6ba36bc70b5c19724fa568a6c0
Ng Guoyou <gyng@users.noreply.github.com>
2020-10-13 21:06:30 +0800

    fix: Don't load start_bot when loaded as a lib

--- lib/betabot.rb
+++ lib/betabot.rb
@@ -1,4 +1,6 @@
 # This file is for using betabot as a gem.
 require_relative('patches/ansi_string')
 require_relative('bot/core')
-Dir[File.expand_path './**/*.rb'].each { |f| require_relative(f) }
+Dir[File.expand_path './lib/*.rb'].each do |f|
+  require_relative(f)
+end
