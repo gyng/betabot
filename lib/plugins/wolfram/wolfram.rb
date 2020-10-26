c0c154246524d06921dfab04e6afaca5d4fefaab
Ng Guoyou <gyng@users.noreply.github.com>
2020-10-27 02:51:12 +0800

    fix(wolfram): Fix wolfram not printing certain results

--- lib/plugins/wolfram/wolfram.rb
+++ lib/plugins/wolfram/wolfram.rb
@@ -33,7 +33,7 @@ class Bot::Plugin::Wolfram < Bot::Plugin
     return if depth > @s[:max_depth]
 
     search_term = CGI.escape(search_term)
-    url = "http://api.wolframalpha.com/v2/query?appid=#{@s[:api_key]}&format=plaintext&input=\'#{search_term}\'"
+    url = "https://api.wolframalpha.com/v2/query?appid=#{@s[:api_key]}&format=plaintext&input=\'#{search_term}\'"
     # rubocop:disable Security/Open
     raw = Nokogiri::XML(open(url))
     # rubocop:enable Security/Open
@@ -43,7 +43,7 @@ class Bot::Plugin::Wolfram < Bot::Plugin
     pods.each do |pod|
       results.push(
         title: pod['title'].strip,
-        text: pod.inner_text.strip.gsub("\n", ' ⋯ '.gray).strip.gsub(/  +/, ' ')
+        text: pod.text.strip.gsub(/\n/, ' ⋯ '.gray).strip.gsub(/  +/, ' ')
       )
     end
 
