6fda88f2f2ba60d829bb3d3e67c96caa1860f1a3
Ng Guoyou <gyng@users.noreply.github.com>
2021-06-06 16:40:05 +0800

    fix(irc): Split text at grapheme boundaries when chunking text into multiple lines

--- lib/adapters/irc/message.rb
+++ lib/adapters/irc/message.rb
@@ -15,14 +15,24 @@ class Bot::Adapter::Irc::Message < Bot::Core::Message
   def reply(text)
     # RFC2812 Section 2.3 - Max of 512 characters (termination \r\n inclusive)
     text.to_s.each_line do |line|
+      line_prefix = "PRIVMSG #{@channel} :"
+      ellipsis = '…'
+      joiner = ' '
+      newline = '\r\n'
       # 512 - 'PRIVMSG #{@channel} :' - '\r\n'
       # max_segment_length = 510 - "PRIVMSG #{@channel} :".length - '...'.length
-      max_segment_length = 400 # Play it safe for non-compliant servers
-      chunks = chunk(line, max_segment_length)
+      max_segment_length_buffer = 30 # Play it safe
+      max_segment_length = 512 - line_prefix.length - newline.length \
+      - joiner.length - ellipsis.length - max_segment_length_buffer
+
+      min_segment_length_buffer = 30
+      min_segment_length = max_segment_length - min_segment_length_buffer
+
+      chunks = chunk(line, min_segment_length, max_segment_length).to_a
 
       chunks.each do |line_segment|
-        reply = "PRIVMSG #{@channel} :#{line_segment}"
-        reply += '...' if line_segment != chunks.last && reply[-1] !~ /[[:punct:]]/
+        reply = "#{line_prefix}#{line_segment}"
+        reply += '…' if line_segment != chunks.last && reply[-1] !~ /[[:punct:]]/
         @origin.send reply
       end
     end
@@ -42,9 +52,49 @@ class Bot::Adapter::Irc::Message < Bot::Core::Message
     args[0]
   end
 
-  private
+  def chunk(input, min_length, max_length)
+    # @param input [String]
+    # @param min_length [Numeric] how long the string should be before starting to look-ahead for chopping
+    # @param max_length [Numeric] how long can the line chopping look-ahead to
+    return to_enum(:chunk, input, min_length, max_length) unless block_given?
 
-  def chunk(str, length)
-    str.scan(/\S.{0,#{length - 1}}(?!\S)/)
+    output_current = ''
+    blank_regex = /[[:blank:]]/
+    blanks = input.scan(blank_regex)
+
+    input.split(blank_regex).each_with_index do |x, i|
+      blank = blanks[i] || ''
+
+      if output_current.bytesize + x.bytesize < min_length
+        output_current += x + blank
+      elsif max_length < output_current.bytesize + x.bytesize
+        parts = split_on_grapheme(output_current + x, max_length).to_a
+        parts[0..-2].each do |part|
+          yield part
+        end
+        output_current = parts[-1]
+      else
+        yield output_current.rstrip
+        output_current = x + blank
+      end
+    end
+
+    # Note: rstrip doesn't handle unicode blanks
+    yield output_current.rstrip if output_current.size.positive?
+  end
+
+  def split_on_grapheme(input, max_length, &_block)
+    return to_enum(:split_on_grapheme, input, max_length) unless block_given?
+
+    output_current = ''
+    input.each_grapheme_cluster do |g|
+      if output_current.bytesize + g.bytesize < max_length
+        output_current += g
+      else
+        yield output_current
+        output_current = ''
+      end
+    end
+    yield output_current if !output_current.empty?
   end
 end
--- spec/bot/adapter/irc_spec.rb
+++ spec/bot/adapter/irc_spec.rb
@@ -5,6 +5,64 @@ require 'adapters/irc/irc'
 require 'adapters/irc/handler'
 require 'adapters/irc/message'
 
+describe Bot::Adapter::Irc::Message do
+  # rubocop:disable Layout/LineLength
+
+  before do
+    [:ROOT_DIR, :SETTINGS_DIR].each do |c|
+      Bot.send(:remove_const, c) if Bot.const_defined?(c)
+    end
+    Bot.const_set(:ROOT_DIR, File.join(Dir.pwd, 'lib'))
+    Bot.const_set(:SETTINGS_DIR, File.join(Dir.pwd, 'lib', 'settings'))
+    @adapter = Bot::Adapter::Irc.new(true)
+    @handler = Bot::Adapter::Irc::Handler.new(@adapter, {})
+  end
+
+  context 'text chunking' do
+    it 'chunks long text at grapheme boundaries' do
+      input = '【三浦建太郎先生　ご逝去の報】 ↵ 『ベルセルク』の作者である三浦建太郎先生が、2021年5月6日、急性大動脈解離のため、ご逝去されました。三浦先生の画業に最大の敬意と感謝を表しますとともに、心よりご冥福をお祈りいたします。 ↵ 2021年5月20日　株式会社白泉社　ヤングアニマル編集部 pic.twitter.com/baBBo4J2kL — ベルセルク公'
+      m = Bot::Adapter::Irc::Message.new
+      chunks = m.chunk(input, 350, 400).to_a
+      expect(chunks.length).to eq(2)
+      # Note trailing whitespace: rstrip doesn't handle unicode blanks
+      expect(chunks[0]).to eq('【三浦建太郎先生　ご逝去の報】 ↵ 『ベルセルク』の作者である三浦建太郎先生が、2021年5月6日、急性大動脈解離のため、ご逝去されました。三浦先生の画業に最大の敬意と感謝を表しますとともに、心よりご冥福をお祈りいたします。 ↵ 2021年5月20日　')
+      expect(chunks[1]).to eq('株式会社白泉社　ヤングアニマル編集部 pic.twitter.com/baBBo4J2kL — ベルセルク公')
+    end
+
+    it 'chunks long text at blank boundaries' do
+      input = 'the quick brown fox jumps over the lazy dog'
+      m = Bot::Adapter::Irc::Message.new
+      chunks = m.chunk(input, 25, 30).to_a
+      expect(chunks.length).to eq(2)
+      expect(chunks[0]).to eq('the quick brown fox')
+      expect(chunks[1]).to eq('jumps over the lazy dog')
+    end
+  end
+
+  context 'message chunjking' do
+    it 'replys with long text broken across multiple PRIVMSGs' do
+      origin = double('origin')
+      expect(origin).to receive(:send) do |x|
+        expect(x).to eq('PRIVMSG channel :Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit…')
+      end
+
+      expect(origin).to receive(:send) do |x|
+        expect(x).to eq('PRIVMSG channel :anim id est laborum.')
+      end
+
+      input = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      msg = Bot::Adapter::Irc::Message.new do |m|
+        m.origin = origin
+        m.channel = 'channel'
+      end
+
+      msg.reply(input)
+    end
+  end
+
+  # rubocop:enable Layout/LineLength
+end
+
 describe Bot::Adapter::Irc::Handler do
   before do
     [:ROOT_DIR, :SETTINGS_DIR].each do |c|
